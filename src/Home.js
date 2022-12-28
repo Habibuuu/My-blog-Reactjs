7ff26e8477ccecd67995948f668067422527e2aa
Muhammad Habiburrahman <habibcoyy@gmail.com>
2022-12-28 09:51:34 +0700

    pertama

---
+++ src/Home.js
@@ -0,0 +1,19 @@
+const Home = () => {
+    const handleClick = (e) => { 
+        console.log('Hello, bibuuu', e); 
+    }
+    const handleClickAgain = (name, e) => { 
+        console.log('Hello ' + name, e.target);
+    }
+    return (
+        <div className="home">
+            <h2>Homepage</h2> 
+            <button onClick={handleClick}>Click Me</button>
+            <button onClick={(e) => {
+                handleClickAgain('Habiburrahman', e) 
+            }}>Click Me Again</button>
+        </div>
+    );
+}
+ 
+export default Home; 
