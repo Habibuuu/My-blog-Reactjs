cdedceeb630b3010d04f29aa3f85c4dbc716d95e
Muhammad Habiburrahman <habibcoyy@gmail.com>
2022-12-28 15:23:34 +0700

    update

--- src/Home.js
+++ src/Home.js
@@ -1,19 +1,31 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
  
-const Home = () => {
-    // let name = 'mario';
+const Home = () => { 
     const [blogs, SetBlogs] = useState([
         { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
         { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
         { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 },
         { title: 'Subscribe my blog', body: 'lorem ipsum...', author: 'habib', id: 4 }
     ]);
+
+    const [name, setName] = useState('Habib');
+
+    const handleDelete = (id) =>{ 
+         const newBlogs = blogs.filter((blog) => blog.id !== id);
+         SetBlogs(newBlogs);
+    }
+    
+    useEffect(() => {
+        console.log('user effect habib');
+        console.log(name);
+    }, [name]);
       
     return (
         <div className="home">
-            <BlogList blogs={blogs} title="All Blogs" />
-            <BlogList blogs={blogs.filter((blog) => blog.author === 'habib')} title="Habib Blogs" />
+            <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete} />
+            <button onClick={() => setName('Bibuu')}>Change Name</button>
+            <p>{ name }</p>
         </div>
     );
 }
