bb573aee8a25687a9303e77a7957b877c333bd83
Muhammad Habiburrahman <habibcoyy@gmail.com>
2022-12-28 14:51:10 +0700

    Update

--- src/Home.js
+++ src/Home.js
@@ -1,19 +1,21 @@
+import { useState } from "react";
+import BlogList from "./BlogList";
+ 
 const Home = () => {
-    const handleClick = (e) => { 
-        console.log('Hello, bibuuu', e); 
-    }
-    const handleClickAgain = (name, e) => { 
-        console.log('Hello ' + name, e.target);
-    }
+    // let name = 'mario';
+    const [blogs, SetBlogs] = useState([
+        { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
+        { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
+        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 },
+        { title: 'Subscribe my blog', body: 'lorem ipsum...', author: 'habib', id: 4 }
+    ]);
+      
     return (
         <div className="home">
-            <h2>Homepage</h2> 
-            <button onClick={handleClick}>Click Me</button>
-            <button onClick={(e) => {
-                handleClickAgain('Habiburrahman', e) 
-            }}>Click Me Again</button>
+            <BlogList blogs={blogs} title="All Blogs" />
+            <BlogList blogs={blogs.filter((blog) => blog.author === 'habib')} title="Habib Blogs" />
         </div>
     );
 }
- 
+  
 export default Home; 
